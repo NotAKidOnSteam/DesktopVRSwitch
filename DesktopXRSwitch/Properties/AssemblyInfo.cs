376dd3451de4e98ede2dc734608d9652f9b1a117
NotAKidoS <37721153+NotAKidOnSteam@users.noreply.github.com>
2023-02-18 10:07:12 -0600

    Update AssemblyInfo.cs

--- DesktopXRSwitch/Properties/AssemblyInfo.cs
+++ DesktopXRSwitch/Properties/AssemblyInfo.cs
@@ -25,6 +25,6 @@ using System.Reflection;
 namespace NAK.Melons.DesktopXRSwitch.Properties;
 internal static class AssemblyInfoParams
 {
-    public const string Version = "4.2.4";
+    public const string Version = "4.3.4";
     public const string Author = "NotAKidoS";
 }
